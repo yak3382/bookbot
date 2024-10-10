0ec4d8c12333d897c1b9120f7a8656b5ed80a602
yak3382 <k.colin83@gmail.com>
2024-10-10 10:17:17 +0200

    created the reading report function including the word count and the sorted character count

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 def main():
+    global book_path
     book_path = "books/frankenstein.txt"
+    global text
     text = get_book_text(book_path)
+    print(f"{text}\n")
+    reading_report()
+
+def reading_report():
     word_count = count_words(text)
-    word_dict = count_characters(text)
-    print(text)
-    print(f"This text is {word_count} words long")
-    print(f"Here is the number of each character: {word_dict}")
+    letter_dict = count_characters(text)
+    print(f"--- Begin report of {book_path} ---\n")
+    print(f"This text is {word_count} words long.\n")
+    for letter in letter_dict:
+        print(f"The '{letter}' character was found {letter_dict[letter]} times.")
+    print()
+    print(f"--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def count_characters(text):
+    text = ''.join(filter(str.isalpha, text.lower()))
     letter_dict = {}
-    for character in text.lower():
+    for character in text:
         if character in letter_dict:
             letter_dict[character] += 1
         else:
             letter_dict[character] = 1
-    return letter_dict
+    dict_keys = list(letter_dict.keys())
+    dict_keys.sort()
+    sorted_dict = {i: letter_dict[i] for i in dict_keys} 
+    return sorted_dict
 
 def count_words(text):
     words = text.split()
     return len(words)
-    
+
 main()
